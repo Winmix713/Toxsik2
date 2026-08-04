bda94d004dcb94c07510b0c5161aa4a48d3cacdd
Winmix713 <takosadam@gmail.com>
2026-08-04 15:02:40 +0200

    Updated main.tsx

--- demo/main.tsx
+++ demo/main.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './tailwind.css';
-import '/demo/index.css';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(<App />);
